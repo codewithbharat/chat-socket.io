eab10b1be8dc0b0561529966740ebc4c09eb9d40
Bharat Ranjan <ranjanbharat54@gamil.com>
2024-10-24 16:38:09 +0530

    input Username

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const io = new Server(server);
 io.on('connection', (socket) => {
     console.log('A user connected');
     socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
+        socket.broadcast.emit('chat message', msg);
     });
     socket.on('disconnect', () => {
         console.log('A user disconnected');
