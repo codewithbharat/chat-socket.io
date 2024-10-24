dd219a8bfd4edc024673a70a8e0007a5df47003c
Bharat Ranjan <ranjanbharat54@gamil.com>
2024-10-24 17:02:13 +0530

    cors integration

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 import express from 'express';
-import {createServer} from 'node:http';
+import { createServer } from 'node:http';
 import { fileURLToPath } from 'node:url';
 import { dirname } from 'node:path';
-
 import { Server } from 'socket.io';
-
-// import dotenv
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 const app = express();
 const server = createServer(app);
 
-const PORT =  process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-// socket.io server
-const io = new Server(server);
+// socket.io server with CORS configuration
+const io = new Server(server, {
+    cors: {
+        origin: "*", // Allow all origins, you can restrict this to specific domains
+        methods: ["GET", "POST"]
+    }
+});
 
 let roomCounter = 0;
 const rooms = {};
@@ -57,15 +60,12 @@ io.on('connection', (socket) => {
     });
 });
 
-
-
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.get('/', (req ,res) => {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-server.listen( PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
-    }
-);
+});
